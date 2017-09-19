6bc23f76649a5d715863c424d61b6be2ad2088fc
Matthew Lombana <mplombana@gmail.com>
2017-09-19 15:06:56 -0400

    initial commit, finished project

---
+++ main.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+    console.log('the page is loaded!');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+
+            var response = JSON.parse(request.responseText);
+            console.log(request.responseText)
+            var weather = response.weather[0].main;
+            // if (weather === "Thunderstorm"){
+            //   document.getElementById('cloud').innerHTML =   <div id = "cloud"><span class='shadow'></span></div>
+            // }
+
+            console.log(weather);
+            var sunrise = response.sys.sunrise;
+            var sunset = response.sys.sunset;
+            var time = new Date().getTime()/1000;
+            var secondsSinceSunrise = time-sunrise;
+            var daylightTime = sunset-sunrise;
+            var percentX = (secondsSinceSunrise/daylightTime)*100;
+            var percentY = -0.035*Math.pow(percentX-52, 2)+80;
+            var sun = document.getElementById('sun');
+
+            sun.style.left = percentX+'%';
+            sun.style.bottom = percentY+'%';
+
+            console.log(percentX);
+        }
+
+    };
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=Miami,FL,us&APPID=54094c5e9c3430a7255880a409cc24af";
+    request.open('GET', url);
+    request.send();
+    // http://api.openweathermap.org/data/2.5/weather?q=Miami,FL,us&APPID=54094c5e9c3430a7255880a409cc24af
+};
